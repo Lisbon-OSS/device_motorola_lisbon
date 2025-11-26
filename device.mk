51174865e0c19a92ee2fa801d27d0b66d548ca6b
Mashopy <eliasgheeraert@gmail.com>
2025-11-26 15:31:49 -0500

    lisbon: Build AOSP composer 2.3 service
    Change-Id: Ib52f4c26b55db247508a591ad2bb71c42b1e9030

--- device.mk
+++ device.mk
@@ -84,7 +84,6 @@ PRODUCT_PACKAGES += \
     libaudiofoundation.vendor \
     libtinycompress
 
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_device.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_device.xml \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
@@ -100,6 +99,16 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
+
+# Display
+PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0.vendor \
+    android.hardware.graphics.allocator@2.0.vendor \
+    android.hardware.graphics.allocator@3.0.vendor \
+    android.hardware.graphics.allocator@4.0.vendor
+
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.3-service
 
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
