6549f8721e42426e60441b8b6104e7da3bae556c
Mashopy <eliasgheeraert@gmail.com>
2025-11-26 15:43:17 -0500

    lisbon: Build OSS keystore related vendor targets
    Change-Id: I8567305758c1ff427c367aebd3a80893673ce534

--- device.mk
+++ device.mk
@@ -136,6 +136,13 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
+# Keystore
+PRODUCT_PACKAGES += \
+    libcppbor_external.vendor \
+    libkeystore-wifi-hidl \
+    libkeystore-engine-wifi-hidl
+
+# Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
