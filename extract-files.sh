94fd3b09695fe18c2a79328000e7595e8b6ba1cc
R0rt1z2 <roger@r0rt1z2.com>
2025-11-30 05:27:39 -0500

    lisbon: Hexpatch camera lib to increase maximum JPEG streams for an app
    * Forcefully increase maxAppJpegStreamNum to 17 in order to fix GCam lags.
    
    Change-Id: I5457132e5b409bc953fd24ce989f0bdfcd58f9d8

--- extract-files.sh
+++ extract-files.sh
@@ -92,6 +92,11 @@ function blob_fixup {
         vendor/lib64/libcam.utils.sensorprovider.so)
             grep -q "libshim_sensors.so" "$2" || "$PATCHELF" --add-needed "libshim_sensors.so" "$2"
             ;;
+        vendor/lib64/libmtkcam_featurepolicy.so)
+            # evaluateCaptureConfiguration()
+            printf '\x28\x02\x80\x52' | dd of="$2" bs=1 seek=$((0x3e828)) count=4 conv=notrunc
+            printf '\x28\x02\x80\x52' | dd of="$2" bs=1 seek=$((0x3e8f4)) count=4 conv=notrunc
+            ;;
     esac
 }
 
