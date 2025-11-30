a6a78737eedc7a9bab5dd203d43c5749cb28d90d
luk1337 <priv.luk@gmail.com>
2025-11-30 10:59:28 -0500

    lisbon: Rework way to prevent adding duplicate dependencies
    Change-Id: I9f104c3b36e3ba5d55915a679ed144d5f19c9950

--- extract-files.sh
+++ extract-files.sh
@@ -75,8 +75,7 @@ function blob_fixup {
             sed -i 's/1.1/1.2/' "$2"
             ;;
         vendor/bin/hw/android.hardware.media.c2@1.2-mediatek | vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
-            "${PATCHELF}" --replace-needed "libavservices_minijail_vendor.so" "libavservices_minijail.so" "${2}"
-            "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
+            grep -q "libstagefright_foundation-v33.so" "${2}" || "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
             ;;
         vendor/lib*/hw/vendor.mediatek.hardware.pq@2.12-impl.so)
             ;&
